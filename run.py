5ac353799757e9684c199ed03e1dc9625517aed2
saramarin23 <saramarin.econ@gmail.com>
2020-04-16 17:42:16 +0200

    add templates

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", num_posts=len(posts))
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
